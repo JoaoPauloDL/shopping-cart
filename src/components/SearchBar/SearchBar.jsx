85dbea1c330a7075d16e0ec9d961b5051ff02c44
João Paulo <joaopaulomayka@gmail.com>
2024-05-16 16:42:09 -0300

    feat: add the searchValue state to receive data from my search input through handleInputChange

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,24 @@
 import { BsSearch } from "react-icons/bs";
 import './SearchBar.css'
+import { useState } from "react";
 
 const SearchBar = () => {
+
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleInputChange = (event) =>{
+    setSearchValue(event.target.value);
+  }
+
+  
   return (
     <form className="search-bar">
       <input
         type="search"
+        value={searchValue}
         placeholder="Buscar produtos"
         className="search__input"
+        onChange={handleInputChange}
         required
       />
       <button type="submit" className="search__button">
