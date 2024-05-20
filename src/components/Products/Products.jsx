96841ffe222b0518025d2f0a01a5e17207eb767f
João Paulo <joaopaulomayka@gmail.com>
2024-05-20 16:56:57 -0300

    feat: add loading spinner to Products component
    - Created a Loading component with a spinning loader animation using `react-icons` and CSS animations.
    - Integrated the Loading component into the Products component to display a loading spinner while products data is being fetched.
    - Updated Products component to conditionally render Loading or ProductCard components based on loading state.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,18 +3,23 @@ import "./Products.css";
 import { useEffect } from "react";
 import fetchProducts from "../../api/fetchProducts";
 import ProductCard from "../ProductCard/ProductCard";
+import Loading from "../Loading/Loading";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchProducts("tecnologia").then((response) => {
       setProducts(response);
+      setLoading(false)
     });
   }, []);
 
   console.log(products);
-  return (
+  return loading ? (
+    <Loading />
+  ) : (
     <section className="products container">
       {products.map((item) => (
         <ProductCard key={item.id} product={item} />
