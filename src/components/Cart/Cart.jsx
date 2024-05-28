f4f2b2ae7a225085f91826cb09d1b1b0c49f2f30
João Paulo <joaopaulomayka@gmail.com>
2024-05-28 17:59:16 -0300

    feat: Implement dynamic display of cart item count and item removal functionality
    - Added dynamic display of the number of items in the cart when adding a new item
    - Implemented functionality to remove an item by clicking on the remove button
    - Added option for the cart menu to appear only when clicking on the cart icon

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,19 +2,22 @@ import { useContext } from "react";
 import AppContext from "../../context/AppContext";
 import CartItem from "../CartItem/CartItem";
 import "./Cart.css";
+import formatCurrency from "../../utils/formatCurrency";
 
 const Cart = () => {
 
-  const { cartItems } = useContext(AppContext);
+  const { cartItems, isCartVisible } = useContext(AppContext);
+  
+  const total = cartItems.reduce((acc, cartItem) => acc + cartItem.price, 0);
 
   return (
-    <section className="cart">
+    <section className={`cart ${isCartVisible ? 'cart--active' : ''}`}>
       <div className="cart-items">
         {
             cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem}/>)
         }
       </div>
-      <div className="cart-resume">resumo do carrinho</div>
+      <div className="cart-resume">{formatCurrency(total)}</div>
     </section>
   );
 };
--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,9 +2,17 @@ import { BsCartDashFill } from "react-icons/bs";
 import propTypes from 'prop-types'
 import "./CartItem.css";
 import formatCurrency from "../../utils/formatCurrency";
+import { useContext } from "react";
+import AppContext from "../../context/AppContext";
 
 const CartItem = ({ data }) => {
 
+  const {cartItems, setCartItems} = useContext(AppContext)
+
+  const handleRemoveItem = () => {
+    const updatedItems = cartItems.filter((item) => item.id != data.id);
+    setCartItems(updatedItems)
+  }
   
 
   return (
@@ -13,7 +21,11 @@ const CartItem = ({ data }) => {
       <div className="cart-item-content">
         <h3 className="cart-item-title">{data.title}</h3>
         <h3 className="cart-item-price">{formatCurrency(data.price)}</h3>
-        <button type="button" className="button__remove-item">
+        <button 
+        type="button" 
+        className="button__remove-item"
+        onClick={handleRemoveItem}
+        >
             <BsCartDashFill/>
         </button>
       </div>
@@ -25,6 +37,7 @@ export default CartItem;
 
 CartItem.propTypes = {
     data: propTypes.shape({
+      id: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
       thumbnail: propTypes.string.isRequired,
       price: propTypes.number.isRequired,
       title: propTypes.string.isRequired,
